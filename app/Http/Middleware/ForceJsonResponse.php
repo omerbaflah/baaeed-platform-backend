92bc46497278cd6003182eac0711517636107b46
Omer Baflah <aldonramos@gmail.com>
2022-10-07 20:17:32 +0300

    Updating force JSON response middleware

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -16,6 +16,11 @@ class ForceJsonResponse
      */
     public function handle(Request $request, Closure $next)
     {
+        $request->headers->set(
+            'Accept',
+            'application/json'
+        );
+
         return $next($request);
     }
 }
