ab78b11e0c358341b6f819bf488a5fcc0bd48bdb
Omer Baflah <aldonramos@gmail.com>
2022-10-08 23:57:47 +0300

    Updating API response helper phase 1

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * success response method.
+ *
+ * @param mixed $message
+ * @param null|mixed $data
+ * @param mixed $status_code
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
+function sendSuccessResponse($message = 'OK', $data = null, $status_code = 200)
+{
+    $response = [
+        'success' => true,
+        'message' => $message,
+        'data' => $data,
+        'status_code' => $status_code,
+    ];
+
+    return response()->json(
+        $response,
+        $status_code
+    );
+}
