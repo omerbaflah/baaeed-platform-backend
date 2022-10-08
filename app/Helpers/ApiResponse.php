a2ff8be20d1c4ab869c18c737fc29e8aa922e5f6
Omer Baflah <aldonramos@gmail.com>
2022-10-08 23:58:30 +0300

    Updating API response helper phase 2

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -23,3 +23,29 @@ function sendSuccessResponse($message = 'OK', $data = null, $status_code = 200)
         $status_code
     );
 }
+
+/**
+ * return error response.
+ *
+ * @param mixed $message
+ * @param null|mixed $data
+ * @param mixed $status_code
+ * @param null|mixed $errors
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
+function sendErrorResponse($message = 'Error', $data = null, $status_code = 404)
+{
+    $response = [
+        'success' => false,
+        'message' => $message,
+        'data' => $data,
+        'status_code' => $status_code
+    ];
+
+    return response()->json(
+        $response,
+        $status_code
+    );
+}
+
