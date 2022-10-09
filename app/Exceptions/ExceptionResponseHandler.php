b78b19b0124a4aeef5e2ea2e450d2974f3433e16
Omer Baflah <aldonramos@gmail.com>
2022-10-09 23:59:52 +0300

    Updating exception response handler

--- app/Exceptions/ExceptionResponseHandler.php
+++ app/Exceptions/ExceptionResponseHandler.php
@@ -2,7 +2,20 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Request;
+use Throwable;
+
 trait ExceptionResponseHandler
 {
-
+    /**
+     * Creates a new JSON response based on exception type.
+     *
+     * @param Request $request
+     * @param Throwable $e
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function getJsonResponseForException(Request $request, Throwable $e)
+    {
+        //
+    }
 }
