8f87e5b765c9e7686bf2ac515393caf40b54e782
Omer Baflah <aldonramos@gmail.com>
2022-10-14 14:50:39 +0300

    Code formatting: exception handler phase 2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -72,6 +72,9 @@ class Handler extends ExceptionHandler
             );
         }
 
-        return parent::render($request, $e);
+        return parent::render(
+            $request,
+            $e
+        );
     }
 }
